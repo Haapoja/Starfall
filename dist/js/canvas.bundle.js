24ffdcc12ed52dc0a384071194dea22e7bbfd31d
Krisu <krisu.97@hotmail.com>
2019-02-15 15:48:10 +0200

    full project

--- dist/js/canvas.bundle.js
+++ dist/js/canvas.bundle.js
@@ -108,18 +108,10 @@ var c = canvas.getContext('2d');
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
-var mouse = {
-    x: innerWidth / 2,
-    y: innerHeight / 2
-};
-
 var colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66'];
 
 // Event Listeners
-addEventListener('mousemove', function (event) {
-    mouse.x = event.clientX;
-    mouse.y = event.clientY;
-});
+
 
 addEventListener('resize', function () {
     canvas.width = innerWidth;
@@ -129,44 +121,175 @@ addEventListener('resize', function () {
 });
 
 // Objects
-function Object(x, y, radius, color) {
+function Star(x, y, radius, color) {
     this.x = x;
     this.y = y;
     this.radius = radius;
-    this.color = color;
+    this.color = "#" + ((1 << 24) * Math.random() | 0).toString(16); //color
+    this.gravity = 1;
+    this.friction = 0.8;
+    this.vel = {
+        x: _utils2.default.randomIntFromRange(-4, 4),
+        y: 3
+    };
 }
 
-Object.prototype.draw = function () {
+Star.prototype.draw = function () {
+    c.save();
     c.beginPath();
     c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
     c.fillStyle = this.color;
+    c.shadowColor = "#E3EAEF";
+    c.shadowBlur = 20;
     c.fill();
     c.closePath();
+    c.restore();
 };
 
-Object.prototype.update = function () {
+Star.prototype.update = function () {
     this.draw();
+    //when ball hits bottom of screen
+    if (this.y + this.radius + this.vel.y > canvas.height - groundHeight) {
+        this.vel.y = -this.vel.y * this.friction;
+        this.shatter();
+    } else {
+        this.vel.y += this.gravity;
+    }
+
+    //if hits side of screen
+    if (this.x + this.radius + this.vel.x > canvas.width || this.x - this.radius <= 0) {
+        this.vel.x = -this.vel.x;
+        this.shatter();
+    }
+    this.x += this.vel.x;
+    this.y += this.vel.y;
 };
+
+Star.prototype.shatter = function () {
+    this.radius -= 1;
+    for (var i = 0; i < 4; i++) {
+        miniStars.push(new miniStar(this.x, this.y, 2));
+    }
+};
+
+function miniStar(x, y, radius, color) {
+    Star.call(this, x, y, radius, color);
+    this.vel = {
+        x: _utils2.default.randomIntFromRange(-5, 5),
+        y: _utils2.default.randomIntFromRange(-15, 15)
+    };
+    this.friction = 0.8;
+    this.gravity = 0.4;
+    this.ttl = 100; //Time to Life
+    this.opacity = 1;
+    this.r = _utils2.default.randomIntFromRange(0, 300);
+    this.g = _utils2.default.randomIntFromRange(0, 300);
+    this.b = _utils2.default.randomIntFromRange(0, 300);
+}
+miniStar.prototype.draw = function () {
+
+    c.save();
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.fillStyle = 'rgba(' + this.r + ', ' + this.g + ', ' + this.b + ', ' + this.opacity + ')';
+    c.shadowColor = "#E3EAEF";
+    c.shadowBlur = 20;
+    c.fill();
+    c.closePath();
+    c.restore();
+};
+
+miniStar.prototype.update = function () {
+    this.draw();
+    //when ball hits bottom of screen
+    if (this.y + this.radius + this.vel.y > canvas.height - groundHeight) {
+        this.vel.y = -this.vel.y * this.friction;
+    } else {
+        this.vel.y += this.gravity;
+    }
+    this.x += this.vel.x;
+    this.y += this.vel.y;
+    this.ttl -= 1;
+    this.opacity -= 1 / this.ttl;
+};
+
+function createMountainRange(mountainAmount, height, color) {
+    for (var i = 0; i < mountainAmount; i++) {
+        var mountainWidth = canvas.width / mountainAmount;
+        c.beginPath();
+        c.moveTo(i * mountainWidth, canvas.height);
+        c.lineTo(i * mountainWidth + mountainWidth + 325, canvas.height);
+        c.lineTo(i * mountainWidth + mountainWidth / 2, canvas.height - height);
+        c.lineTo(i * mountainWidth - 325, canvas.height);
+        c.fillStyle = color;
+        c.fill();
+        c.closePath();
+    }
+}
 
 // Implementation
-var objects = void 0;
+var backgroundGradient = c.createLinearGradient(0, 0, 0, canvas.height); //top to bottom
+backgroundGradient.addColorStop(0, "#171e26");
+backgroundGradient.addColorStop(1, "#3f586b");
+var stars = void 0;
+var miniStars = void 0;
+var bgStars = void 0;
+var ticker = 0;
+var randomSpawnRate = 135;
+var groundHeight = 70;
 function init() {
-    objects = [];
-
-    for (var i = 0; i < 400; i++) {
-        // objects.push();
+    stars = [];
+    miniStars = [];
+    bgStars = [];
+    for (var i = 0; i < 1; i++) {
+        stars.push(new Star(canvas.width / 2, 30, 15, "#E3EAEF"));
+    }
+    for (var _i = 0; _i < 100; _i++) {
+        var x = Math.random() * canvas.width;
+        var y = Math.random() * canvas.height;
+        var radius = Math.random() * 3;
+        bgStars.push(new Star(x, y, radius, "white"));
     }
 }
 
 // Animation Loop
 function animate() {
     requestAnimationFrame(animate);
-    c.clearRect(0, 0, canvas.width, canvas.height);
+    c.fillStyle = backgroundGradient;
+    c.fillRect(0, 0, canvas.width, canvas.height);
 
-    c.fillText('HTML CANVAS BOILERPLATE', mouse.x, mouse.y);
-    // objects.forEach(object => {
-    //  object.update();
-    // });
+    bgStars.forEach(function (bgStars) {
+        bgStars.draw();
+    });
+
+    createMountainRange(1, canvas.height - 50, "#384551");
+    createMountainRange(2, canvas.height - 100, "#2B3843");
+    createMountainRange(3, canvas.height - 300, "#26333E");
+    c.fillStyle = "#182028";
+    c.fillRect(0, canvas.height - groundHeight, canvas.width, groundHeight);
+
+    stars.forEach(function (star, index) {
+        star.update();
+        if (star.radius == 0) {
+            stars.splice(index, 1);
+        }
+    });
+    miniStars.forEach(function (miniStar, index) {
+        miniStar.update();
+        if (miniStar.ttl == 0) {
+            miniStars.splice(index, 1);
+        }
+    });
+
+    ticker++;
+
+    if (ticker % randomSpawnRate == 0) {
+        var radius = 15;
+        var x = Math.max(radius, Math.random() * canvas.width - radius);
+
+        stars.push(new Star(x, -100, radius, "white"));
+        randomSpawnRate = _utils2.default.randomIntFromRange(75, 100);
+    }
 }
 
 init();
--- src/canvas.js
+++ src/canvas.js
@@ -6,18 +6,10 @@ const c = canvas.getContext('2d')
 canvas.width = innerWidth
 canvas.height = innerHeight
 
-const mouse = {
-    x: innerWidth / 2,
-    y: innerHeight / 2
-}
-
 const colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66']
 
 // Event Listeners
-addEventListener('mousemove', event => {
-    mouse.x = event.clientX
-    mouse.y = event.clientY
-})
+
 
 addEventListener('resize', () => {
     canvas.width = innerWidth
@@ -27,44 +19,183 @@ addEventListener('resize', () => {
 })
 
 // Objects
-function Object(x, y, radius, color) {
+function Star(x, y, radius, color) {
     this.x = x
     this.y = y
     this.radius = radius
-    this.color = color
+    this.color = "#" + ((1 << 24) * Math.random() | 0).toString(16) //color
+    this.gravity = 1
+    this.friction = 0.8
+    this.vel = {
+        x: utils.randomIntFromRange(-4, 4),
+        y: 3
+    }
 }
 
-Object.prototype.draw = function() {
+Star.prototype.draw = function () {
+    c.save()
     c.beginPath()
     c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
     c.fillStyle = this.color
+    c.shadowColor = "#E3EAEF"
+    c.shadowBlur = 20
     c.fill()
     c.closePath()
+    c.restore()
 }
 
-Object.prototype.update = function() {
+Star.prototype.update = function () {
     this.draw()
+    //when ball hits bottom of screen
+    if (this.y + this.radius + this.vel.y > canvas.height - groundHeight) {
+        this.vel.y = -this.vel.y * this.friction
+        this.shatter()
+
+    } else {
+        this.vel.y += this.gravity
+    }
+
+    //if hits side of screen
+    if (this.x + this.radius + this.vel.x > canvas.width || this.x - this.radius <= 0) {
+        this.vel.x = -this.vel.x
+        this.shatter()
+    }
+    this.x += this.vel.x
+    this.y += this.vel.y
+}
+
+Star.prototype.shatter = function () {
+    this.radius -= 1
+    for (let i = 0; i < 4; i++) {
+        miniStars.push(new miniStar(this.x, this.y, 2))
+    }
+}
+
+function miniStar(x, y, radius, color) {
+    Star.call(this, x, y, radius, color)
+    this.vel = {
+        x: utils.randomIntFromRange(-5, 5),
+        y: utils.randomIntFromRange(-15, 15)
+    }
+    this.friction = 0.8
+    this.gravity = 0.4
+    this.ttl = 100  //Time to Life
+    this.opacity = 1
+    this.r = utils.randomIntFromRange(0, 300)
+    this.g = utils.randomIntFromRange(0, 300)
+    this.b = utils.randomIntFromRange(0, 300)
+
+}
+miniStar.prototype.draw = function () {
+
+    c.save()
+    c.beginPath()
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+    c.fillStyle = `rgba(${this.r}, ${this.g}, ${this.b}, ${this.opacity})`
+    c.shadowColor = "#E3EAEF"
+    c.shadowBlur = 20
+    c.fill()
+    c.closePath()
+    c.restore()
+}
+
+miniStar.prototype.update = function () {
+    this.draw()
+    //when ball hits bottom of screen
+    if (this.y + this.radius + this.vel.y > canvas.height - groundHeight) {
+        this.vel.y = -this.vel.y * this.friction
+    } else {
+        this.vel.y += this.gravity
+    }
+    this.x += this.vel.x
+    this.y += this.vel.y
+    this.ttl -= 1
+    this.opacity -= 1 / this.ttl
+
+}
+
+function createMountainRange(mountainAmount, height, color) {
+    for (let i = 0; i < mountainAmount; i++) {
+        const mountainWidth = canvas.width / mountainAmount
+        c.beginPath()
+        c.moveTo(i * mountainWidth, canvas.height)
+        c.lineTo(i * mountainWidth + mountainWidth + 325, canvas.height)
+        c.lineTo(i * mountainWidth + mountainWidth / 2, canvas.height - height)
+        c.lineTo(i * mountainWidth - 325, canvas.height)
+        c.fillStyle = color
+        c.fill()
+        c.closePath()
+
+    }
 }
 
 // Implementation
-let objects
+const backgroundGradient = c.createLinearGradient(0, 0, 0, canvas.height) //top to bottom
+backgroundGradient.addColorStop(0, "#171e26")
+backgroundGradient.addColorStop(1, "#3f586b")
+let stars
+let miniStars
+let bgStars
+let ticker = 0
+let randomSpawnRate = 135
+const groundHeight = 70
 function init() {
-    objects = []
-
-    for (let i = 0; i < 400; i++) {
-        // objects.push();
+    stars = []
+    miniStars = []
+    bgStars = []
+    for (let i = 0; i < 1; i++) {
+        stars.push(new Star(canvas.width / 2, 30, 15, "#E3EAEF"))
+    }
+    for (let i = 0; i < 100; i++) {
+        const x = Math.random() * canvas.width
+        const y = Math.random() * canvas.height
+        const radius = Math.random() * 3
+        bgStars.push(new Star(x, y, radius, "white"))
     }
 }
 
 // Animation Loop
 function animate() {
     requestAnimationFrame(animate)
-    c.clearRect(0, 0, canvas.width, canvas.height)
+    c.fillStyle = backgroundGradient
+    c.fillRect(0, 0, canvas.width, canvas.height)
 
-    c.fillText('HTML CANVAS BOILERPLATE', mouse.x, mouse.y)
-    // objects.forEach(object => {
-    //  object.update();
-    // });
+
+
+
+
+    bgStars.forEach((bgStars => {
+        bgStars.draw()
+    }))
+
+    createMountainRange(1, canvas.height - 50, "#384551")
+    createMountainRange(2, canvas.height - 100, "#2B3843")
+    createMountainRange(3, canvas.height - 300, "#26333E")
+    c.fillStyle = "#182028"
+    c.fillRect(0, canvas.height - groundHeight, canvas.width, groundHeight)
+
+    stars.forEach((star, index) => {
+        star.update();
+        if (star.radius == 0) {
+            stars.splice(index, 1)
+        }
+    });
+    miniStars.forEach((miniStar, index) => {
+        miniStar.update();
+        if (miniStar.ttl == 0) {
+            miniStars.splice(index, 1)
+        }
+    })
+
+    ticker++
+
+    if (ticker % randomSpawnRate == 0) {
+        const radius = 15
+        const x = Math.max(radius, Math.random() * canvas.width - radius)
+
+        stars.push(new Star(x, -100, radius, "white"))
+        randomSpawnRate = utils.randomIntFromRange(75, 100)
+    }
 }
 
 init()
